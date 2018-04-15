70a6267ee42715c1e44ab73a031d6bb617d9d54d
Richard Li <lifuzu+bepro@gmail.com>
2018-04-14 17:23:38 -0700

    :bug: Config localhost for both platforms
    Config localhost for both platforms

--- clients/reactnative/bepro/socket.js
+++ clients/reactnative/bepro/socket.js
@@ -1,11 +1,15 @@
 import openSocket from 'socket.io-client';
-const  socket = openSocket('http://localhost:3000/time');
+import { Platform } from 'react-native';
+
+const API_HOST = Platform.OS === 'ios' ? 'localhost' : '10.0.2.2'
+
+const  socket = openSocket('http://' + API_HOST + ':3000/time');
 function subscribeToTimer(cb) {
   socket.on('timer', timestamp => cb(null, timestamp));
   socket.emit('subscribeToTimer', 1000);
 }
 
-const wechat = openSocket('http://localhost:3000/wechat');
+const wechat = openSocket('http://' + API_HOST + ':3000/wechat');
 function scanToWechat(cb) {
   wechat.on('scan', url => cb(null, url));
 }
